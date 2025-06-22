41f843160425ba912fb1c274bbe0c83074c8675d
andromeda0004 <akhilbj04@gmail.com>
2025-06-23 02:05:04 +0530

    drivers imported

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#code begins
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+cdp = "/home/kali/Desktop/Chrome Drivers/chromedriver-linux64/chromedriver"
+service = Service(executable_path=cdp)
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.google.com")
+driver.quit()
