cf9f1d1b2fdf984fda6923f65ca0b98df4a6102a
Akhil <144477575+andromeda0004@users.noreply.github.com>
2025-06-26 21:08:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 cdp = "/home/kali/Desktop/Chrome Drivers/chromedriver-linux64/chromedriver" #this is my address. your path can change.
-service = Service(executable_path=cdp)
-driver = webdriver.Chrome(service=service)
+serv = Service(executable_path=cdp)
+driver = webdriver.Chrome(service=serv)
 
 driver.get("https://www.google.com")
 driver.quit()
